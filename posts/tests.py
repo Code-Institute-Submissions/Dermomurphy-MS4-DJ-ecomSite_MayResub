5f958163a4c476b52def25dd13396fdb0500a9a6
Dermot Murphy <dermotmurphy@Dermots-MacBook-Pro.local>
2021-02-27 12:40:33 +0000

    added setup tests for product and post

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
-
+from posts.models import Posts
+from datetime import datetime
 # Create your tests here.
+
+class PostTestCase(TestCase):
+    def setUp(self):
+        Posts.objects.create(title='Test1', body='TEst text',created_on=datetime.now)
--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from products.models import Product, Brewery
 
 # Create your tests here.
+
+class ProductTestCase(TestCase):
+    def setUp(self):
+        Products.objects.create(brewery='Brewery1', sku='pp999999',name='Test Name',style='Lager', description='Text Description', abv=3.0,ibu=29, price=3.99,rating=3.45,image_url='https://image.com', image='noimage.png')
+
