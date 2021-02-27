c6109e6bdf5596193c46ac8704f131b66117cabb
Dermot Murphy <dermotmurphy@Dermots-MacBook-Pro.local>
2021-02-27 21:20:00 +0000

    heroku deployment settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ecomSite.wsgi.application
--- ecomSite/settings.py
+++ ecomSite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 if os.path.exists("env.py"):
     import env
 
@@ -29,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ecom-nearrocks.herokuapp.com','localhost']
 
 
 # Application definition
@@ -153,12 +155,19 @@ WSGI_APPLICATION = 'ecomSite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+if 'DATABASE_URL' in os.environ
+    DATABASES = {
+    'default':  dj_database_url.parse(os.environ.get('DATABASE_URL'))
+
 }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 # Password validation
