5d585e42db721289d4d2213f331a2aeecabd1cec
Dermot Murphy <dermotmurphy@Dermots-MacBook-Pro.local>
2021-04-24 21:10:24 +0100

    updated shopping bag style and functionality

--- ecomSite/settings.py
+++ ecomSite/settings.py
@@ -215,7 +215,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 FREE_DELIVERY_THRESHOLD = 20
-STANDARD_DELIVERY_PERCENTAGE = 10
+STANDARD_DELIVERY_PERCENTAGE = 15
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
