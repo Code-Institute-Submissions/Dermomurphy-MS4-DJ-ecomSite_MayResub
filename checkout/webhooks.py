ef460dbb920ea403b031657999d28d23b76009cd
Dermot Murphy <dermotmurphy@Dermots-MacBook-Pro.local>
2021-05-01 18:21:24 +0100

    added class methods to webhook handler and webhook view

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -31,7 +31,22 @@ def webhook(request):
         # Exception
         return HttpResponse(status=400)
     
-    print('Success')
-    return HttpResponse(status=200)
+   # Set up a webhook handler
+    handler = StripeWH_Handler(request)
 
+   #map webhook events to relevant handler functions
+    event_map = {
+       'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
+       'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
+        }
  
+   # get the webhook type from stripe
+    event_type = event['type']
+
+    # if there a handler for it, get it from the event map
+    # use the generic one by default
+    event_handler = event_map.get(event_type, handler.handle_event)
+
+    #call the even handler with the event
+    response = event_handler(event)
+    return response
