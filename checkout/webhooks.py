83cbcb8239a45f9b267e6141351c94dab554d616
Dermot Murphy <dermotmurphy@Dermots-MacBook-Pro.local>
2021-05-01 18:02:42 +0100

    added WH secret to Herouku fixed spelling mistake in webhooks.py file re djang

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from djang.conf import settings
+from django.conf import settings
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 from checkout.webhook_handler import StripeWH_Handler
