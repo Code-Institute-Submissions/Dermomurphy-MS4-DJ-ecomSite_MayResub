8b6fb88402827b20dcb2b447cfc91d802808092b
Dermot Murphy <dermotmurphy@Dermots-MacBook-Pro.local>
2021-05-02 18:10:51 +0100

    fix typo error on event_map in webhooks.py

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -35,7 +35,7 @@ def webhook(request):
     handler = StripeWH_Handler(request)
 
    #map webhook events to relevant handler functions
-    çevent_map = {
+    event_map = {
        'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
         }
